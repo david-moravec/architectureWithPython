c8f93c729f5eedc41c57b7beb2c045a6ac786f95
David Moravec <moravec@numsolution.cz>
2023-04-16 13:01:28 +0200

    reformat

--- tests/test_domainmodel.py
+++ tests/test_domainmodel.py
@@ -2,17 +2,21 @@ from domainmodel import Order, Batch
 
 from pytest import raises, fixture
 
+
 @fixture
 def order():
-    return Order(sku='test', quantity=2)
+    return Order(sku="test", quantity=2)
+
 
 @fixture
 def order1():
-    return Order(sku='est', quantity=1)
+    return Order(sku="est", quantity=1)
+
 
 def test_order_post_init():
     with raises(ValueError):
         a = Order(sku="test", quantity=-1)
+
 
 def test_order_sub(order, order1):
     order2 = Order(sku="test", quantity=1)
@@ -25,6 +29,7 @@ def test_order_sub(order, order1):
     assert order2.sku == order.sku
     assert order3.sku == order.sku
     assert order3.quantity == order.quantity - order2.quantity
+
 
 def test_order_eq(order, order1):
     order2 = order
