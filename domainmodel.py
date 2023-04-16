f15205e89dbe83ac49370cfe3fd21ffbe3e5f2b3
David Moravec <moravec@numsolution.cz>
2023-04-16 14:59:24 +0200

    Add functionality to batch, add utests

--- domainmodel.py
+++ domainmodel.py
@@ -1,13 +1,17 @@
-from ast import Raise
+from typing import Optional
+from datetime import date
+
 from dataclasses import dataclass
 
 
-from enum import Enum
+class SkuErorr(Exception):
+    pass
+
 
 @dataclass(frozen=True, kw_only=True)
 class Order:
-    sku: str = None
-    quantity: int = 0
+    sku: str
+    quantity: int
 
     def __post_init__(self) -> None:
         if self.quantity < 0:
@@ -18,35 +22,61 @@ class Order:
             return self.quantity == o.quantity
 
         return False
-        
+
     def __sub__(self, o: "Order") -> "Order":
         if not self.same_SKU(o):
-            raise ValueError('Incompatible SKU')
+            raise ValueError("Incompatible SKU")
 
         difference: int = self.quantity - o.quantity
-        
+
         return Order(sku=self.sku, quantity=difference)
 
     def __str__(self) -> str:
-        return f'SKU = {self.sku}, quantity = {self.quantity}'
-        
+        return f"SKU = {self.sku}, quantity = {self.quantity}"
+
+    def __hash__(self) -> int:
+        return self.orderid
 
     def same_SKU(self, o: "Order") -> bool:
         return self.sku == o.sku
 
 
+class OrderLine(Order):
+    orderid: int
+
+
 class Batch:
-    def __init__(self, reference: int, contents: Order) -> None:
+    def __init__(self, reference: int, contents: Order, eta: Optional[date]) -> None:
         self.reference = reference
-        self.contents = contents
+        self.available = contents
+        self.eta = eta
         self._past_orders: set[Order] = set()
+
+    def compatible_sku(self, order: Order) -> bool:
+        return self.available.same_SKU(order)
+
+    def add_available_quantity(self, to_add: Order) -> None:
+        if not self.compatible_sku(to_add):
+            return
+
+        available = self.available
+
+        self.available = Order(
+            sku=available.sku, quantity=available.quantity + to_add.quantity
+        )
 
     def allocate_order(self, order: Order) -> bool:
         if order in self._past_orders:
             return False
 
-        self.contents = self.contents - order
+        self.available = self.available - order
 
         self._past_orders.add(Order)
 
         return True
+
+    def can_allocate(self, order: Order) -> bool:
+        if self.compatible_sku(order):
+            return self.available.quantity >= order.quantity
+
+        return False
--- tests/test_domainmodel.py
+++ tests/test_domainmodel.py
@@ -1,38 +1,69 @@
-from domainmodel import Order, Batch
+from domainmodel import Order, Batch, SkuErorr
 
 from pytest import raises, fixture
 
 
-@fixture
-def order():
-    return Order(sku="test", quantity=2)
+def make_batch_orderline(sku, batch_qty, line_qty):
+    return Batch(
+        reference=1, contents=Order(sku=sku, quantity=batch_qty), eta=""
+    ), Order(sku=sku, quantity=line_qty)
 
 
 @fixture
-def order1():
-    return Order(sku="est", quantity=1)
+def order_big():
+    return Order(sku="test", quantity=20)
+
+
+@fixture
+def order_small():
+    return Order(sku="est", quantity=5)
 
 
 def test_order_post_init():
     with raises(ValueError):
-        a = Order(sku="test", quantity=-1)
+        Order(sku="test", quantity=-1)
 
 
-def test_order_sub(order, order1):
+def test_order_sub(order_big, order_small):
     order2 = Order(sku="test", quantity=1)
 
     with raises(ValueError):
-        order3 = order - order1
+        order3 = order_big - order_small
 
-    order3 = order - order2
+    order3 = order_big - order2
 
-    assert order2.sku == order.sku
-    assert order3.sku == order.sku
-    assert order3.quantity == order.quantity - order2.quantity
+    assert order2.sku == order_big.sku
+    assert order3.sku == order_big.sku
+    assert order3.quantity == order_big.quantity - order2.quantity
 
 
-def test_order_eq(order, order1):
-    order2 = order
+def test_order_eq(order_big, order_small):
+    order2 = order_big
 
-    assert order1 != order
-    assert order2 == order
+    assert order_small != order_big
+    assert order2 == order_big
+
+
+def test_batch_add_available_quantity(order_big):
+    b = Batch(reference=1, contents=order_big, eta=None)
+    b.add_available_quantity(order_big)
+
+    assert b.available.quantity == 2 * order_big.quantity
+
+
+def test_can_allocate_if_available_greater_than_requested():
+    large_batch, small_order = make_batch_orderline("ELEGANT-LAMP", 20, 10)
+
+    assert large_batch.can_allocate(small_order)
+
+
+def test_cannot_allocate_if_available_smaller_than_requested():
+    small_batch, large_order = make_batch_orderline("ELEGANT-LAMP", 10, 20)
+
+    assert small_batch.can_allocate(large_order) is False
+
+
+def test_raise_error_if_sku_non_compatible(order_small):
+    batch, _ = make_batch_orderline("TEST", 20, 10)
+
+    assert batch.can_allocate(order_small) is False
